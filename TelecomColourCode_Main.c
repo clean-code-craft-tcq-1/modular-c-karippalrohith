e6e483ab9f085d3c662b54da16d6909f6feda796
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-02-22 19:33:51 +0530

    Update TelecomColourCode_Main.c

--- TelecomColourCode_Main.c
+++ TelecomColourCode_Main.c
@@ -39,30 +39,6 @@
  =======                              METHODS                            =======
  =============================================================================*/
  
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s",
-        MajorColorNames_a[colorPair->majorColor],
-        MinorColorNames_a[colorPair->minorColor]);
-}
-
-int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * numberOfMinorColors +
-            colorPair->minorColor + 1;
-}
-
-void testPairToNumber(
-    enum MajorColor_e major,
-    enum MinorColor_e minor,
-    int expectedPairNumber)
-{
-    ColorPair colorPair;
-    colorPair.majorColor = major;
-    colorPair.minorColor = minor;
-    int pairNumber = GetPairNumberFromColor(&colorPair);
-    printf("Got pair number %d\n", pairNumber);
-    assert(pairNumber == expectedPairNumber);
-}
-
 int main() {
     //test
     testNumberToPair(4, WHITE, BROWN);
