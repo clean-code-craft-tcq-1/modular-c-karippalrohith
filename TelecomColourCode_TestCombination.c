2043fcfa9b9fc034e3ad36dd9c8bc70918ca15c4
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-02-22 18:53:48 +0530

    Create TelecomColourCode_TestCombination.c

---
+++ TelecomColourCode_TestCombination.c
@@ -0,0 +1,49 @@
+/* ******************************************************************************
+ ***
+ ***     \file        main.c
+ ***     \ingroup     Session1-TCQ
+ ***     \author      Rohith K N
+ ***
+ ***     \brief       Source file to explore more about code modularity
+ ***
+ ***     \par         File_description
+ ***                  This file explores more about code modularity
+ ***
+ *****************************************************************************/
+ 
+/*=============================================================================
+ =======                            INCLUDES                             =======
+ ============================================================================*/
+ /*------ standard includes -------*/
+#include <stdio.h>
+
+ /*------ module includes -------*/
+#include "TelecomColourCode_TestCombination.h"
+
+void ColorPairToString1(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames_a[colorPair->majorColor],
+        MinorColorNames_a[colorPair->minorColor]);
+}
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor_e)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor_e)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
+
+void testNumberToPair(int pairNumber,
+    enum MajorColor_e expectedMajor,
+    enum MinorColor_e expectedMinor)
+{
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString1(&colorPair, colorPairNames);
+    printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
