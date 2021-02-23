38ccedcad8bd486c285d056492f151162a912852
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-02-23 22:04:52 +0530

    Update TelecomColourCode_Test_PairNumber.c

--- TelecomColourCode_Test_PairNumber.c
+++ TelecomColourCode_Test_PairNumber.c
@@ -81,21 +81,3 @@ void TCC_testPairNumber_AgaintColours(int pairNumber,
     assert(colorPair.majorColor == expectedMajor);
     assert(colorPair.minorColor == expectedMinor);
 }
-
-/*---------------------------------------------------------------------------*/
-/*     FUNCTION: TCC_printColourCodeReference_Table
- */
-/*!    \brief   Function to derive the Pair code from the major and Minor colours
- *
- *
- *     \param    colourIndex_i -> This varies from 1-25 for all the combinations
- *     \returns  void
- *
- *//*------------------------------------------------------------------------*/
-void TCC_printColourCodeReference_Table(int colourIndex_i)
-{
-    ColorPair_s colorPair = TCC_GetColorFromPairNumber(colourIndex_i);
-    char colorPairNames[MAX_COLORPAIR];
-    TCC_ColorPairToString(&colorPair, colorPairNames);
-    printf("Pair %d\t\t%s\n", colourIndex_i, colorPairNames);
-}
