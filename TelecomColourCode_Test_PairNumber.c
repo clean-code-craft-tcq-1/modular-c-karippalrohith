77f297f6e1ed6123e8ad91addce04fdd8da68756
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-02-23 19:43:17 +0530

    Update TelecomColourCode_Test_PairNumber.c

--- TelecomColourCode_Test_PairNumber.c
+++ TelecomColourCode_Test_PairNumber.c
@@ -54,5 +54,5 @@ void printColourCodeReference_Table(int colourIndex)
     ColorPair colorPair = GetColorFromPairNumber(colourIndex);
     char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
     ColorPairToString(&colorPair, colorPairNames);
-    printf("Pair %d\t%s\n", colourIndex, colorPairNames);
+    printf("Pair %d\t\t\t%s\n", colourIndex, colorPairNames);
 }
