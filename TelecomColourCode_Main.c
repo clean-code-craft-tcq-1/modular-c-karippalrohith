08f0744a669de00f6f83b10094c857af2ae9d2ef
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-02-23 22:00:21 +0530

    Update TelecomColourCode_Main.c

--- TelecomColourCode_Main.c
+++ TelecomColourCode_Main.c
@@ -4,7 +4,7 @@
  ***     \ingroup     Session1-TCQ
  ***     \author      Rohith K N
  ***
- ***     \brief       Source file to explore more about code modularity
+ ***     \brief       Main file to check different colour combinations
  ***
  ***     \par         File_description
  ***                  This file explores more about code modularity
@@ -36,31 +36,43 @@ const char* MinorColorNames_a[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
 
-int numberOfMajorColors_i = sizeof(MajorColorNames_a) / sizeof(MajorColorNames_a[0]);
-int numberOfMinorColors_i = sizeof(MinorColorNames_a) / sizeof(MinorColorNames_a[0]);
 /*=============================================================================
  =======                VARIABLES & MESSAGES & RESSOURCEN                =======
  ==============================================================================*/
- 
+int numberOfMajorColors_i = sizeof(MajorColorNames_a) / sizeof(MajorColorNames_a[0]);
+int numberOfMinorColors_i = sizeof(MinorColorNames_a) / sizeof(MinorColorNames_a[0]);
 
 /*=============================================================================
  =======                              METHODS                            =======
  =============================================================================*/
  
+/*---------------------------------------------------------------------------*/
+/*     FUNCTION: main
+ */
+/*!    \brief   Function to test colour combinations from colour code and vice versa.
+ *				It also prints the colour combination table 
+ *
+ *
+ *     \param    none
+ *     \returns  int
+ *
+ *//*------------------------------------------------------------------------*/
 int main() {
     int colourIndex_i;
  
-    testNumberToPair(4, WHITE, BROWN);
-    testNumberToPair(5, WHITE, SLATE);
+	//Test the colour pair code against the expected Major and Minor colour of the respective code
+    TCC_testPairNumber_AgaintColours(4, WHITE, BROWN);
+    TCC_testPairNumber_AgaintColours(5, WHITE, SLATE);
 
-    testPairToNumber(BLACK, ORANGE, 12);
-    testPairToNumber(VIOLET, SLATE, 25);
+	//Test the Major and Minor colour to the respective colour code
+    TCC_testColours_AgainstPairNumber(BLACK, ORANGE, 12);
+    TCC_testColours_AgainstPairNumber(VIOLET, SLATE, 25);
     
-    /*Print Colour Reference*/
+    /*Print Colour Reference for all the 25 Pairs*/
     printf("Colour Code   Major Colour::Minor Colour\n");
-    for(colourIndex_i = 1; colourIndex_i <= MAX_COLORPAIR_NAME_CHARS; colourIndex_i++)
+    for(colourIndex_i = 1; colourIndex_i <= MAX_COLORPAIR; colourIndex_i++)
     {
-        printColourCodeReference_Table(colourIndex_i);
+        TCC_printColourCodeReference_Table(colourIndex_i);
     }
  
     return 0;
