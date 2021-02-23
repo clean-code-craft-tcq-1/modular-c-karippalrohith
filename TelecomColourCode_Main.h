5f7014631f946832a581b36bbb15a43a926a40ed
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-02-23 22:00:42 +0530

    Update TelecomColourCode_Main.h

--- TelecomColourCode_Main.h
+++ TelecomColourCode_Main.h
@@ -3,10 +3,10 @@
  ***     \ingroup     Session1-TCQ
  ***     \author      Rohith K N
  ***
- ***     \brief       Source file to explore more about code modularity
+ ***     \brief       Header file for the module
  ***
  ***     \par         File_description
- ***                  This file explores more about code modularity
+ ***                  This file have the declarations of the variables and methods of the module
  ***
  *****************************************************************************/
 #ifndef TELECOM_COLOR_CODE_MAIN_H_
@@ -16,27 +16,29 @@
  ============================================================================*/
  /*------ standard includes -------*/
 #include <stdio.h>
-#define MAX_COLORPAIR_NAME_CHARS 25
+
+#define MAX_COLORPAIR 				25		/*maximum number of colour pairs possible*/
+
 enum MajorColor_e{
-	WHITE, 				/*0*/
-	RED, 				/*1*/
-	BLACK, 				/*2*/
-	YELLOW,             		/*3*/
-	VIOLET              		/*4*/
+	WHITE, 									/*0*/
+	RED, 									/*1*/
+	BLACK, 									/*2*/
+	YELLOW,             					/*3*/
+	VIOLET              					/*4*/
 };
 
 enum MinorColor_e{
-	BLUE, 			/*0*/
-	ORANGE,             	/*1*/
-	GREEN,              	/*2*/
-	BROWN,              	/*3*/
-	SLATE               	/*4*/
+	BLUE, 									/*0*/
+	ORANGE,             					/*1*/
+	GREEN,              					/*2*/
+	BROWN,              					/*3*/
+	SLATE               					/*4*/
 };
 
 typedef struct {
     enum MajorColor_e majorColor;
     enum MinorColor_e minorColor;
-} ColorPair;
+} ColorPair_s;
 
 extern const char* MajorColorNames_a[];
 
@@ -45,14 +47,10 @@ extern const char* MinorColorNames_a[];
 extern int numberOfMajorColors_i;
 extern int numberOfMinorColors_i;
 
-void testPairToNumber(enum MajorColor_e major, enum MinorColor_e minor, int expectedPairNumber);
-void testNumberToPair(int pairNumber, enum MajorColor_e expectedMajor, enum MinorColor_e expectedMinor);
-
-void testNumberToPair(int pairNumber, enum MajorColor_e expectedMajor, enum MinorColor_e expectedMinor);
-ColorPair GetColorFromPairNumber(int pairNumber);
-void ColorPairToString(const ColorPair* colorPair, char* buffer);
-
-void testPairToNumber(enum MajorColor_e major, enum MinorColor_e minor, int expectedPairNumber);
-int GetPairNumberFromColor(const ColorPair* colorPair);
-void printColourCodeReference_Table(int colourIndex_i);
+void TCC_testColours_AgainstPairNumber(enum MajorColor_e major, enum MinorColor_e minor, int expectedPairNumber);
+void TCC_testPairNumber_AgaintColours(int pairNumber, enum MajorColor_e expectedMajor, enum MinorColor_e expectedMinor);
+ColorPair_s TCC_GetColorFromPairNumber(int pairNumber);
+void TCC_ColorPairToString(const ColorPair_s* colorPair, char* buffer);
+int TCC_GetPairNumberFromColor(const ColorPair_s* colorPair);
+void TCC_printColourCodeReference_Table(int colourIndex_i);
 #endif /*TELECOM_COLOR_CODE_MAIN_H_*/
